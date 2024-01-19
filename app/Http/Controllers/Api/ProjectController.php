e505440ef85161b093bdc135322c6abac723252d
giuliabaratella <giuliabaratella7@gmail.com>
2024-01-19 10:40:36 +0100

    added api project controller /updated api routes/ added index and show functions

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::paginate(5);
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+
+    }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', $slug)->with(['category', 'technologies'])->first();
+        return response()->json([
+            'success' => true,
+            'results' => $project
+        ]);
+    }
+}
